e5876053cdb7e9fbb1c3baad6a899ed95604ff62
Nigel <nigel@thegordons.id.au>
2020-09-08 09:26:15 +0800

    First commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Amplify from '@aws-amplify/core';
+import App from './App.svelte';
+import aws_exports from './aws-exports';
+
+// in this way you are only importing Auth and configuring it.
+Amplify.configure(aws_exports);
+
+const app = new App({
+  target: document.body
+});
+
+export default app;
---
+++ src/models/schema.js
@@ -0,0 +1,41 @@
+export const schema = {
+    "models": {
+        "Todo": {
+            "name": "Todo",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "done": {
+                    "name": "done",
+                    "isArray": false,
+                    "type": "Boolean",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "description": {
+                    "name": "description",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Todos",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "105098061cfb237adb35ad0c239c8b1e"
+};
